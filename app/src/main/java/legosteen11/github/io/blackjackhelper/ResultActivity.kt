9c369af8f399e69bb4a7af0304a1501e9285a4a5
wouter <wouterdoeland@gmail.com>
2017-11-30 18:45:22 +0100

    Added a logo, extra styling and colours

--- app/src/main/java/legosteen11/github/io/blackjackhelper/ResultActivity.kt
+++ app/src/main/java/legosteen11/github/io/blackjackhelper/ResultActivity.kt
@@ -3,6 +3,7 @@ package legosteen11.github.io.blackjackhelper
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import io.github.legosteen11.blackjacklib.Game
 import kotlinx.android.synthetic.main.activity_result.*
 
@@ -15,12 +16,16 @@ class ResultActivity : AppCompatActivity() {
 
         game = intent.getSerializableExtra("game_result") as Game
 
+        // set background color
+        val backgroundColor = if(game.getWon()) R.color.colorWin else R.color.colorLost
+        findViewById<View>(android.R.id.content).setBackgroundResource(backgroundColor)
+
         val scores = game.getScore()
 
         game_result.text = if(game.getWon()) "Gewonnen!" else "Verloren..."
 
         score.text = "Je ${if(scores.size > 1) "scores zijn" else "score is"}: ${scores.sortedBy { it }.joinToString()}"
-        cards.text = "Je kaarten waren: ${game.getPlayersCards().joinToString()}"
+        cards.text = "Je kaarten waren: ${game.getPlayersCards().joinToString { "${it.name} (${it.values.joinToString()})" }}"
     }
 
     override fun onBackPressed() {
